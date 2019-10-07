790d43e36b90eb2bc65e20c1da399315f672b188
pittvandewitt <thomasdewitt@outlook.com>
2019-10-07 17:17:42 +0200

    Implement logout button

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import SignupFormContainer from './components/SignupFormContainer';
 import LoginFormContainer from './components/LoginFormContainer';
+import Logout from './components/Logout';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          Pictionary
+          <h2>Pictionary</h2>
+          {this.props.user.jwt && <Logout />}
           <Route path="/signup" component={SignupFormContainer} />
           <Route path="/login" component={LoginFormContainer} />
         </div>
@@ -18,4 +21,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (props) => {
+  return {user: props.user}
+}
+
+export default connect(mapStateToProps)(App);
