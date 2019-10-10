72be9ff60007eb2b5092e996d16f8e3e64accb25
pittvandewitt <thomasdewitt@outlook.com>
2019-10-10 10:16:35 +0200

    Implement routing

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../actions/userActions'
+import { withRouter } from 'react-router'
 
 class Logout extends Component {
 
-  onClick = (event) => {
+  onClick = () => {
     this.props.logout()
+    this.props.history.push('/login')
   }
 
   render() {
-    return (
-      <div>
-        <button onClick={this.onClick}>Log out</button>
-      </div>
-    )
+    return <div>
+      <button onClick={this.onClick}>Log out</button>
+    </div>
   }
 }
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
